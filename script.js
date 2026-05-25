9e1aa4bbe040e8829a6278aac1fec8261be07d35
Quixvy <tjpareja330@gmail.com>
2026-05-25 18:15:36 +0800

    feat: Added DOM and reworked the js logic

--- script.js
+++ script.js
@@ -9,53 +9,57 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    return prompt("Enter a move (rock, paper, scissors)").toLowerCase()
-}
-
 function playround(humanChoice,computerChoice) {
     if (humanChoice === "rock" && computerChoice === "scissors") {
-        console.log("You Win! Rock beats Scissors")
+        displayWin.textContent = "You Win! Rock beats Scissors";
         humanScore ++
     } else if (humanChoice === "rock" && computerChoice === "paper") {
-        console.log("You Lose! Paper beats Rock")
+        displayWin.textContent = "You Lose! Paper beats Rock";
         computerScore ++
     } else if (humanChoice === "paper" && computerChoice === "rock") {
-        console.log("You Win! Paper beats Rock")
+        displayWin.textContent = "You Win! Paper beats Rock";
         humanScore ++
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        console.log("You Lose! Scissors beats Paper")
+        displayWin.textContent = "You Lose! Scissors beats Paper";
         computerScore ++
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        console.log("You Win! Scissors beats Paper")
+        displayWin.textContent = "You Win! Scissors beats Paper";
         humanScore ++
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        console.log("You Lose! Rock beats Scissors")
+        displayWin.textContent = "You Lose! Rock beats Scissors";
         computerScore ++
     } else if (humanChoice === computerChoice) {
-        console.log("Tie! Both have the same move")
+        displayWin.textContent ="Tie! Both have the same move";
     }
 }
 
-function playGame() {
-    for (let i = 1; i<=5; i++) {
-        playround(getHumanChoice(),getComputerChoice())
-    }
-    console.log(`Your Score is: ${humanScore}`)
-    console.log(`The Computer Score is: ${computerScore}`)
-    if (humanScore > computerScore) {
-        console.log("You Win!")
-    } else if (humanScore < computerScore) {
-        console.log("You Lose!")
-    } else if (humanScore === computerScore) {
-        console.log("It's a Tie Game!")
-    }
+function displayScore() {
+    scoreDisplay.textContent = `Your Score is: ${humanScore} and the Computer Score is: ${computerScore}`;
 }
+
+function play(move) {
+    const playerMove = move;
+    const computerMove = getComputerChoice(); 
+    displayMove.textContent = `Your Move is: ${playerMove} and The Computer Move is: ${computerMove}`;
+    playround(playerMove, computerMove);
+    displayScore();
+}
+
+const scoreDisplay = document.querySelector(".displayScore"); 
+const displayMove = document.querySelector(".displayMove");
+const displayWin = document.querySelector(".displayWin");
+const btnRock = document.querySelector("#buttonRock");
+const btnPaper = document.querySelector("#buttonPaper");
+const btnScissors = document.querySelector("#buttonScissors");
+
+
+btnRock.addEventListener("click", () => play("rock"));
+btnPaper.addEventListener("click", () => play("paper"));
+btnScissors.addEventListener("click", () => play("scissors"));
 
 let humanScore = 0
 let computerScore = 0
 
 
-playGame()
 
 
